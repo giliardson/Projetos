5bcc405461d6b0f2463993eda554904d0be4048a
Giliardson Cezar <98276270+giliardson@users.noreply.github.com>
2022-07-27 16:08:05 -0300

    new

---
+++ calculadora/script.js
@@ -0,0 +1,31 @@
+var tela = window.document.getElementById('tela')
+
+function calculadora(x){
+    tela.innerText = `${tela.innerText}${x}`
+    teste.innerText = tela.innerText.length
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var teste = window.document.getElementById('teste')
